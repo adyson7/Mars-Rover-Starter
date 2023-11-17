39a23dc9b9687523450fd00f06e4f6e2d2b6f04e
AD <142710797+adyson7@users.noreply.github.com>
2023-11-16 19:25:45 -0600

    Mars Rover Part 1 & 2 passed

--- message.js
+++ message.js
@@ -1,6 +1,12 @@
 class Message {
   constructor(name,commands) {
+   if (!name) {
+      throw new Error('Message name required.');
+    }
 
+    this.name = name;
+    this.commands = commands || [];
+  }
   }
 }
 
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,36 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+   
+  it("throws error if name is NOT passed into constructor as the first parameter", function() {
+    expect(function() { new Message(); }).toThrow(new Error('Message name required.'));
+  });
+
+  it("constructor sets name", function() {
+    let message = new Message('TestMessage');
+    expect(message.name).toEqual('TestMessage');
+  });
+
+  it("constructor sets commands", function() {
+    let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 10)];
+    let message = new Message('TestMessage', commands);
+    expect(message.commands).toEqual(commands);
+  });
+
+  it("constructor sets received time", function() {
+    let receivedTime = new Date();
+    let message = new Message('TestMessage', [], receivedTime);
+    expect(message.receivedTime).toEqual(receivedTime);
+  });
+
+  it("commands property is an array", function() {
+    let message = new Message('TestMessage');
+    expect(Array.isArray(message.commands)).toBe(true);
+  });
+
+  it("commands property defaults to an empty array if not provided", function() {
+    let message = new Message('TestMessage');
+    expect(message.commands.length).toEqual(0);
+  });
 
 });
